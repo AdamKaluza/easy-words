f91f326f78b553226fdc751139f207c9542a2e95
Adam Kaluza <adam.kaluza.97@gmail.com>
2021-06-06 16:27:26 +0200

    implement words service

--- src/app/services/words.service.ts
+++ src/app/services/words.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Type, WordType} from "../data/models";
+import {WORDS} from "../data/data-base";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordsService {
 
-  constructor() { }
+  private words: WordType[] = [];
+  private nouns: WordType[] = [];
+  private verbs: WordType[] = [];
+
+  constructor() {
+    this.words = WORDS;
+  }
+
+  getWords(): WordType[] {
+    return this.words;
+  }
+
+  getNouns(): WordType[] {
+    return this.nouns;
+  }
+
+  getVerbs(): WordType[] {
+    return this.verbs;
+  }
+
+  addNouns(value: WordType): void {
+    this.nouns.push(value);
+  }
+
+  addVerb(value: WordType): void {
+    this.verbs.push(value);
+  }
+
+  check() {
+    this.nouns.map(word => (word.correct = word.type === Type.NOUN));
+    this.verbs.map(word => (word.correct = word.type === Type.VERB));
+  }
+
+
 }
